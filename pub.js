3831b2dd91f45722f6827992a272d4794b34539a
Petra696 <91752761+Petra696@users.noreply.github.com>
2021-11-26 13:13:58 +0100

    Add files via upload

---
+++ pub.js
@@ -0,0 +1,44 @@
+console.log('Pubregels voor jongeren');
+// Leeftijd om alcohol te mogen drinken
+var myAge = { age: "18" };
+console.log(myAge)
+
+// Check of persoon 18 jaar of ouder is
+var myAge = 10;
+if (myAge == 18) {
+    console.log("18 jaar, je mag naar binnen");
+} else if (myAge > 18) {
+    console.log("Ouder dan 18, je mag wel naar binnen");
+} else {
+    console.log("Geen 18, je mag niet naar binnen");
+}
+
+// Check of persoon een vrouw is
+var isFemale = false;
+console.log(typeof isFemale);
+
+//Geslacht vrouw bepalen
+var isFemale = true;
+var Female = 1;
+if (isFemale = 1) {
+    console.log("Vrouw");
+} else if (isFemale < 1) {
+    console.log("Man");
+} else if (isFemale > 1) {
+    console.log("telt niet mee")
+} else {
+    console.log("niets om te tellen");
+}
+
+//Driverstatus
+var driverstatus = { driverstatus: 2 };
+console.log(driverstatus);
+
+// wel of niet rijden
+var driverstatus = true;
+var bob = 2
+if (driverstatus = 2) {
+    console.log("je mag rijden");
+} else {
+    console.log("je mag niet rijden");
+}
